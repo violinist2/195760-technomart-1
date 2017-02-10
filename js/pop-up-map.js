5d9e0bf739de6a63e31a13134be1eedd90393a0c
violinist2 <nepoymaesh@yandex.ru>
2017-02-10 22:08:40 +0300

    Update pop-up-map.js

--- js/pop-up-map.js
+++ js/pop-up-map.js
@@ -1,5 +1,4 @@
-  var mapOpen = document.querySelector(".js-open-map");
-
+      var mapOpen = document.querySelector(".js-open-map");
       var mapPopup = document.querySelector(".modal-content-map");
       var mapClose = mapPopup.querySelector(".map-close");
 
@@ -20,3 +19,44 @@
           }
         }
       });
+
+      
+
+      var letterOpen = document.querySelector(".btn-feedback");
+      var popUpLetter = document.querySelector(".pop-up-letter");
+      var letterClose = popUpLetter.querySelector(".pop-up-basket-close");
+
+      letterOpen.addEventListener("click", function(event) {
+        event.preventDefault();
+        popUpLetter.classList.add("modal-content-show-2");
+      });
+
+      letterClose.addEventListener("click", function(event) {
+        event.preventDefault();
+        popUpLetter.classList.remove("modal-content-show-2");
+      });
+
+      window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (popUpLetter.classList.contains("modal-content-show-2")) {
+            popUpLetter.classList.remove("modal-content-show-2");
+          }
+        }
+      });
+
+
+           if (window.localStorage) {
+                var elements = document.querySelectorAll('[name]');
+
+                for (var i = 0, length = elements.length; i < length; i++) {
+                    (function(element) {
+                        var name = element.getAttribute('name');
+
+                        element.value = localStorage.getItem(name) || '';
+
+                        element.onkeyup = function() {
+                            localStorage.setItem(name, element.value);
+                        };
+                    })(elements[i]);
+                }
+            }
